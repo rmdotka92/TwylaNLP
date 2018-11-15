96c8a86de8e19bc50e81c3c36249257d01a4e417
NyxoutforHarambe <rahuldharamdasani92@gmail.com>
2018-11-15 01:37:03 +0100

    Initial pytest file.

---
+++ test_main.py
@@ -0,0 +1,70 @@
+"""
+Unit tests for main.py
+Run with:
+$ py.test -f test_sorter.py -v
+"""
+import sys
+import main
+import pytest
+
+
+# @pytest.mark.skipif(sys.version_info < (3,0) ,reason = 'Not tested to run on python versions < 3.0')
+def test_pre_processing_tokens():
+    case, _, dummy = main.pre_processing('I love to eat apples')
+    expected = ['I', 'love', 'to', 'eat', 'apples']
+    assert case == expected
+
+def test_pre_processing_postags():
+    _, case, dummy = main.pre_processing('I love to eat apples')
+    expected = ['PRON', 'VERB', 'PART', 'VERB', 'NOUN']
+    assert case == expected
+
+def test_pre_processing_tokenpostags():
+    _, dummy, case = main.pre_processing('I love to eat apples')
+    expected = [('I', 'PRON'), ('love', 'VERB'), ('to', 'PART'), ('eat', 'VERB'),('apples', 'NOUN')]
+    assert case == expected
+
+def test_pre_processing_notstring():
+    case = main.pre_processing(['I love to eat apples'])
+    expected = 405
+    assert case == expected
+
+def test_symspell_test_suggestions():
+    # Example : Boxer and the dog cn't jump over the fenc
+    case, _ = main.symspell_test(
+    [('Boxer', 'PROPN'), ('and', 'CCONJ'), ('the', 'DET'), ('dog', 'NOUN'),
+     ("cn't", 'VERB'), ('jump', 'VERB'), ('over', 'ADP'), ('the', 'DET'),
+      ('fenc', 'NOUN')]
+    )
+    expected = ['Boxer', 'and', 'the', 'dog', "can't", 'jump', 'over', 'the', 'fence']
+    assert case == expected
+
+def test_symspell_test_intactwords():
+    # Example : Boxer and the dog cn't jump over the fenc
+    _, case = main.symspell_test(
+    [('Boxer', 'PROPN'), ('and', 'CCONJ'), ('the', 'DET'), ('dog', 'NOUN'),
+     ("cn't", 'VERB'), ('jump', 'VERB'), ('over', 'ADP'), ('the', 'DET'),
+      ('fenc', 'NOUN')]
+    )
+    expected = ['Boxer']
+    assert case == expected
+
+def test_symspell_test_notlistoftups():
+    case = main.symspell_test("Boxer and the dog cn't jump over the fenc")
+    expected = 410
+    assert case == expected
+
+def test_enchant_check_suggestions():
+    case,_ = main.enchant_check(
+    [('Boxer', 'PROPN'), ('and', 'CCONJ'), ('the', 'DET'), ('dog', 'NOUN'),
+     ("cn't", 'VERB'), ('jump', 'VERB'), ('over', 'ADP'), ('the', 'DET'),
+      ('fenc', 'NOUN')]
+    )
+    expected = ['Boxer', 'and', 'the', 'dog', "can't", 'jump', 'over', 'the', 'enc']
+    assert case == expected
+
+def test_enchant_check_notlistoftups():
+    # Example : Boxer and the dog cn't jump over the fenc
+    case = main.enchant_check("Boxer and the dog cn't jump over the fenc")
+    expected = 410
+    assert case == expected
